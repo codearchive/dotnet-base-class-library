1eea53f11901b804c95ce8630f693651cc949786
Pavel Patrusov <pavelp9415@gmail.com>
2019-11-25 23:19:47 +0200

    Working with Files and Streams in C#. 2 - Managing Files and Directories : 2 - Creating a New Console App

---
+++ files-and-streams-in-c-sharp/module_01/DataProcessor/DataProcessor/FileProcessor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DataProcessor
+{
+    internal class FileProcessor
+    {
+        private string InputFilePath { get; }
+
+        public FileProcessor(string filePath)
+        {
+            InputFilePath = filePath;
+        }
+
+        public void Process()
+        {
+            Console.WriteLine($"Begin process of {InputFilePath}");
+        }
+    }
+}
---
+++ files-and-streams-in-c-sharp/module_01/DataProcessor/DataProcessor/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DataProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Parsing command line options");
+
+            var command = args[0];
+
+            if (command == "--file")
+            {
+                var filePath = args[1];
+                Console.WriteLine($"Single file {filePath} selected");
+                ProcessSingleFile(filePath);
+            }
+            else if (command == "--dir")
+            {
+                var directoryPath = args[1];
+                var fileType = args[2];
+                Console.WriteLine($"Directory {directoryPath} selected for {fileType} files");
+                ProcessDirectory(directoryPath, fileType);
+            }
+            else
+            {
+                Console.WriteLine("Invalid command line options");
+            }
+
+            Console.WriteLine("Press enter to quit");
+            Console.ReadLine();
+        }
+
+        private static void ProcessDirectory(string directoryPath, string fileType)
+        {
+            
+        }
+
+        private static void ProcessSingleFile(string filePath)
+        {
+            var fileProcessor = new FileProcessor(filePath);
+            fileProcessor.Process();
+        }
+    }
+}
