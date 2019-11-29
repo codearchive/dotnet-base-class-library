d8ef829b29fcb0326a164fc8c580f3e75cdc0312
Pavel Patrusov <pavelp9415@gmail.com>
2019-11-29 23:59:37 +0200

    Working with Files and Streams in C#. 3 - Monitoring the File System for Changes : 6 - Refactoring to Use a FileSystemWatcher

---
+++ files-and-streams-in-c-sharp/module_02/DataProcessor/DataProcessor/FileProcessor.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+
+namespace DataProcessor
+{
+    internal class FileProcessor
+    {
+        private static readonly string BackupDirectoryName = "backup";
+        private static readonly string InProgressDirectoryName = "processing";
+        private static readonly string CompletedDirectoryName = "completed";
+
+        private string InputFilePath { get; }
+
+        public FileProcessor(string filePath)
+        {
+            InputFilePath = filePath;
+        }
+
+        public void Process()
+        {
+            Console.WriteLine($"Begin process of {InputFilePath}");
+
+            if (!File.Exists(InputFilePath))
+            {
+                Console.WriteLine($"ERROR: file {InputFilePath} does not exist");
+                return;
+            }
+
+            string rootDirectoryPath = new DirectoryInfo(InputFilePath).Parent.Parent.FullName;
+            Console.WriteLine($"Root data path is {rootDirectoryPath}");
+
+            string inputFileDirectoryPath = Path.GetDirectoryName(InputFilePath);
+            Console.WriteLine($"inputFileDirectoryPath: {inputFileDirectoryPath}");
+            string backupDirectoryPath = Path.Combine(rootDirectoryPath, BackupDirectoryName);
+
+            //if (!Directory.Exists(backupDirectoryPath))
+            //{
+            //    Console.WriteLine($"Creating {backupDirectoryPath}");
+                Directory.CreateDirectory(backupDirectoryPath);
+            //}
+
+            string inputFileName = Path.GetFileName(InputFilePath);
+            string backupFilePath = Path.Combine(backupDirectoryPath, inputFileName);
+            Console.WriteLine($"Copying {InputFilePath} to {backupFilePath}");
+            File.Copy(InputFilePath, backupFilePath, true);
+
+            Directory.CreateDirectory(Path.Combine(rootDirectoryPath, InProgressDirectoryName));
+            string inProgressFilePath = Path.Combine(rootDirectoryPath, InProgressDirectoryName, inputFileName);
+
+            if (File.Exists(inProgressFilePath))
+            {
+                Console.WriteLine($"ERROR: file with the name {inProgressFilePath} is already being processed");
+                return;
+            }
+
+            Console.WriteLine($"Moving {InputFilePath} to {inProgressFilePath}");
+            File.Move(InputFilePath, inProgressFilePath);
+
+            string extension = Path.GetExtension(InputFilePath);
+            switch (extension)
+            {
+                case ".txt":
+                    ProcessTextFile(inProgressFilePath);
+                    break;
+                default:
+                    Console.WriteLine($"{extension} is an unsupported file type.");
+                    break;
+            }
+
+            string completedDirectoryPath = Path.Combine(rootDirectoryPath, CompletedDirectoryName);
+            Directory.CreateDirectory(completedDirectoryPath);
+            Console.WriteLine($"Moving {inProgressFilePath} to {completedDirectoryPath}");
+            var completedFileName = $"{Path.GetFileNameWithoutExtension(InputFilePath)}-{Guid.NewGuid()}{extension}";
+
+            completedFileName = Path.ChangeExtension(completedFileName, ".completed");
+
+            var completedFilePath = Path.Combine(completedDirectoryPath, completedFileName);
+            File.Move(inProgressFilePath, completedFilePath);
+
+            string inProgressDirectoryPath = Path.GetDirectoryName(inProgressFilePath);
+            Directory.Delete(inProgressDirectoryPath, true);
+        }
+
+        private void ProcessTextFile(string inProgressFilePath)
+        {
+            Console.WriteLine($"Processing text file {inProgressFilePath}");
+        }
+    }
+}
---
+++ files-and-streams-in-c-sharp/module_02/DataProcessor/DataProcessor/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace DataProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Parsing command line options");
+
+            var directoryToWatch = args[0];
+
+            if (!Directory.Exists(directoryToWatch))
+            {
+                Console.WriteLine($"ERROR: {directoryToWatch} does not exist");
+            }
+            else
+            {
+                Console.WriteLine($"Watching directory {directoryToWatch} for changes");
+                using (var inputFileWatcher = new FileSystemWatcher(directoryToWatch))
+                {
+                    inputFileWatcher.IncludeSubdirectories = false;
+                    inputFileWatcher.InternalBufferSize = 32768;
+                    inputFileWatcher.Filter = "*.*"; // by default
+                    inputFileWatcher.NotifyFilter = NotifyFilters.LastWrite;
+
+                    inputFileWatcher.Created += FileCreated;
+                    inputFileWatcher.Changed += FileChanged;
+                    inputFileWatcher.Deleted += FileDeleted;
+                    inputFileWatcher.Renamed += FileRenamed;
+                    inputFileWatcher.Error += WatcherError;
+
+                    inputFileWatcher.EnableRaisingEvents = true;
+
+                    Console.WriteLine("Press enter to quit");
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        private static void WatcherError(object sender, ErrorEventArgs e)
+        {
+            Console.WriteLine($"ERROR: file system watching may no longer be active: {e.GetException()}");
+        }
+    
+
+        private static void FileRenamed(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine($"* File Renamed: {e.OldName} to {e.Name} - type: {e.ChangeType}");
+        }
+    
+
+        private static void FileDeleted(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"* File Deleted: {e.Name} - type: {e.ChangeType}");
+        }
+
+        private static void FileChanged(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"* File Changed: {e.Name} - type: {e.ChangeType}");
+        }
+
+        private static void FileCreated(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"* File created: {e.Name} - type: {e.ChangeType}");
+        }
+
+        private static void ProcessDirectory(string directoryPath, string fileType)
+        {
+            //var allFiles = Directory.GetFiles(directoryPath);
+
+            switch (fileType)
+            {
+                case "TEXT":
+                    string[] textFiles = Directory.GetFiles(directoryPath, "*.txt");
+                    foreach (var textFilePath in textFiles)
+                    {
+                        var fileProcessor = new FileProcessor(textFilePath);
+                        fileProcessor.Process();
+                    }
+                    break;
+                default:
+                    Console.WriteLine($"ERROR: {fileType} is not supported");
+                    return;
+            }
+        }
+
+        private static void ProcessSingleFile(string filePath)
+        {
+            var fileProcessor = new FileProcessor(filePath);
+            fileProcessor.Process();
+        }
+    }
+}
