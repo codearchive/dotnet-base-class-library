c09d8f0658e4eb62194ee23cea64aa48323220cd
Pavel Patrusov <pavelp9415@gmail.com>
2019-12-14 23:31:25 +0200

    7 - Creating Automated Tests for File Access Code : 6 - Adding a BinaryFileProcessor Test

---
+++ files-and-streams-in-c-sharp/module_07/DataProcessor/DataProcessor.Tests/BinaryFileProcessorShould.cs
@@ -0,0 +1,31 @@
+﻿using System.IO.Abstractions.TestingHelpers;
+using Xunit;
+
+namespace DataProcessor.Tests
+{
+    public class BinaryFileProcessorShould
+    {
+        [Fact]
+        public void AddLargestNumber()
+        {
+            var mockFileInput = new MockFileData(new byte[] { 0xFF, 0x34, 0x56, 0xD2 });
+            var mockFileSystem = new MockFileSystem();
+            mockFileSystem.AddFile(@"d:\root\in\myfile.data", mockFileInput);
+            mockFileSystem.AddDirectory(@"d:\root\out");
+
+            var sut = new BinaryFileProcessor(@"d:\root\in\myfile.data",
+                @"d:\root\out\myfile.data", mockFileSystem);
+
+            sut.Process();
+
+            Assert.True(mockFileSystem.FileExists(@"d:\root\out\myfile.data"));
+
+            MockFileData processedFile = mockFileSystem.GetFile(@"d:\root\out\myfile.data");
+
+            byte[] data = processedFile.Contents;
+
+            Assert.Equal(5, data.Length);
+            Assert.Equal(0xFF, data[4]);
+        }
+    }
+}
